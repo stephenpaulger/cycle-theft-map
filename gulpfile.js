890ce12d49fca7bccfdfb86d6cee695a5ec9a596
Stephen Paulger <stephen.paulger@arm.com>
2017-08-18 07:37:10 +0100

    gulpfile improvements

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,36 @@
 var gulp = require('gulp'),
     minifyCss = require("gulp-minify-css"),
     uglify = require('gulp-uglify'),
-    useref = require('gulp-useref');
+    useref = require('gulp-useref'),
+    gulpif = require('gulp-if'),
+    connect = require('gulp-connect');
 
-gulp.task('minify-css', function () {
+
+gulp.task('minify', function() {
     var assets = useref.assets();
 
-    return gulp.src('src/map.html')
+    return gulp.src('src/*.html')
         .pipe(assets)
-        .pipe(minifyCss())
+        .pipe(gulpif('*.css', minifyCss()))
+        .pipe(gulpif('*.js', uglify()))
         .pipe(assets.restore())
         .pipe(useref())
-        .pipe(gulp.dest('dist/'));
+        .pipe(gulp.dest('dist'))
+        .pipe(connect.reload());
 });
 
-gulp.task('minify-js', function () {
-    return gulp.src('src/assets/main.js')
-        .pipe(uglify())
-        .pipe(gulp.dest('dist/assets/'));
+
+gulp.task('serve', function() {
+    connect.server({
+        livereload: true
+    });
 });
+
+
+gulp.task('watch', function() {
+    gulp.watch('src/assets/*.css', ['minify']);
+    gulp.watch('src/assets/*.js', ['minify']);
+});
+
+
+gulp.task('default', ['minify', 'serve', 'watch']);
